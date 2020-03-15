9a44b467be8f7254b641e56f8bd3e20ca2f549d5
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 19:45:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,10 @@ class Song
     self.find_by_name(name) != nil ? self.find_by_name(name) : self.create_by_name(name)
   end
 
+  def self.alphabetical(song)
   
+  
+  end
   
   def self.all
     @@all
